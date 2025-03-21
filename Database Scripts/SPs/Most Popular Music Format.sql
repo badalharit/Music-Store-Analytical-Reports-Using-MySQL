d215e6cb9abca080ee0bbef283fd68fdeb62d271
badalharit <badalharitmrt@gmail.com>
2025-03-21 19:53:40 +0530

    SP Added: Most Popular Music Format

---
+++ Database Scripts/SPs/Most Popular Music Format.sql
@@ -0,0 +1,25 @@
+-- Most Popular Music Format
+DELIMITER //
+
+CREATE PROCEDURE GetMediaSales(IN selected_media_type VARCHAR(255))
+BEGIN
+    -- Check if 'All' is selected, then ignore the media_type filter
+    IF selected_media_type = 'All' THEN
+        SELECT m.name AS media_type, COUNT(t.track_id) AS total_tracks_sold
+        FROM tbl_invoice_line il
+        JOIN tbl_track t ON il.track_id = t.track_id
+        JOIN tbl_media_type m ON t.media_type_id = m.media_type_id
+        GROUP BY m.media_type_id
+        ORDER BY total_tracks_sold DESC;
+    ELSE
+        SELECT m.name AS media_type, COUNT(t.track_id) AS total_tracks_sold
+        FROM tbl_invoice_line il
+        JOIN tbl_track t ON il.track_id = t.track_id
+        JOIN tbl_media_type m ON t.media_type_id = m.media_type_id
+        WHERE m.name = selected_media_type
+        GROUP BY m.media_type_id
+        ORDER BY total_tracks_sold DESC;
+    END IF;
+END //
+
+DELIMITER ;
