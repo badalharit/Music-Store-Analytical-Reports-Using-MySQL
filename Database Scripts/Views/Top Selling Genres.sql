5d1c91b709c142ab2f285cb482c0c6eaf1884ca6
badalharit <badalharitmrt@gmail.com>
2025-03-24 20:57:56 +0530

    Added the `id` in the view output for Django model-view compatibility

--- Database Scripts/Views/Top Selling Genres.sql
+++ Database Scripts/Views/Top Selling Genres.sql
@@ -2,12 +2,18 @@
 
 CREATE VIEW vw_top_genres AS
 SELECT 
-    g.genre_id,
+    ROW_NUMBER() OVER (ORDER BY SUM(il.unit_price * il.quantity) DESC) AS id,
+    g.genre_id AS genre_id,
     g.name AS genre_name,
     COUNT(il.invoice_line_id) AS total_purchases,
     SUM(il.unit_price * il.quantity) AS total_revenue
-FROM tbl_invoice_line il
-JOIN tbl_track t ON il.track_id = t.track_id
-JOIN tbl_genre g ON t.genre_id = g.genre_id
-GROUP BY g.genre_id, genre_name
-ORDER BY total_revenue DESC;
+FROM 
+    tbl_invoice_line il
+JOIN 
+    tbl_track t ON il.track_id = t.track_id
+JOIN 
+    tbl_genre g ON t.genre_id = g.genre_id
+GROUP BY 
+    g.genre_id, g.name
+ORDER BY 
+    total_revenue DESC;
